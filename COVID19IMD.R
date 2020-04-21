54197eb53c001aa3adc3a5f860a1a0f3d32b129d
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-04-21 09:58:25 +0100

    Overhauled with new data source

--- COVID19IMD.R
+++ COVID19IMD.R
@@ -8,33 +8,29 @@ library(sf)
 library(gganimate)
 library(cowplot)
 library(lubridate)
+library(data.table)
 
 #Read in COVID dase data
 temp <- tempfile()
-source <- "https://fingertips.phe.org.uk/documents/Historic%20COVID-19%20Dashboard%20Data.xlsx"
+source <- "https://coronavirus.data.gov.uk/downloads/csv/coronavirus-cases_latest.csv"
 temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
-#ZZ is set as the maximum range to capture future dates withou needing to rewrite
-data <- read_excel(temp, sheet="UTLAs", range="A9:ZZ160", col_names=TRUE)
-#remove empty columns from dates in the future
-data <- data[!sapply(data, function (x) all(is.na(x)))]
-#colnames(data)[c(3:26)] <- as.Date(seq(43899:43922), origin="1899-12-30")
-colnames(data) <- c("code", "name", format(seq(from=as.Date(dmy("09/03/20")), 
-                                        to=as.Date(dmy("09/03/20"))+days(ncol(data)-2), by=1), "%d/%m/%y"))
+data <- fread(temp)
+colnames(data) <- c("name", "code", "type", "date", "cases", "cumul_cases")
+data$date <- as.Date(data$date)
 
-data_long <- gather(data, date, cases, c(3:ncol(data)))
-data_long$date <- dmy(data_long$date)
+#split out regional data
+reg_data <- subset(data, type=="Region")
+data <- subset(data, type=="Upper tier local authority")
 
 #Read in IMD data
 temp <- tempfile()
 source <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/834001/File_11_-_IoD2019_Local_Authority_District_Summaries__upper-tier__.xlsx"
 temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
-IMDdata <- read_excel(temp, sheet="IMD", range="A1:D152", col_names=TRUE)
-colnames(IMDdata) <-  c("code", "name", "IMDrank", "IMDorder")
-IMDdata$decile <- ntile(IMDdata$IMDrank, 10)
+IMDdata <- read_excel(temp, sheet="IMD", range="A1:D152", col_names=TRUE)[,-c(2)]
+colnames(IMDdata) <-  c("code", "IMDrank", "IMDorder")
 IMDdata$quintile <- ntile(IMDdata$IMDrank, 5)
 
-#Combine
-data_long <- merge(data_long, IMDdata[,c(1,5,6)], by="code")
+data <- merge(data, IMDdata, by="code")
 
 #Bring in region data (probably a better source for this)
 temp <- tempfile()
@@ -44,16 +40,18 @@ RegionLookup <- read_excel(temp, sheet="UTLA males", range="A10:E160", col_names
 colnames(RegionLookup) <- c("code", "region")
 
 #Combine
-data_long <- merge(data_long, RegionLookup, all.x=TRUE)
+data <- merge(data, RegionLookup, all.x=TRUE)
+
 #Sort out regions with boundary changes since the region list was compiled
-data_long$region <- case_when(
-  data_long$name=="Dorset" ~ "South West",
-  data_long$name=="Bournemouth, Christchurch and Poole" ~ "South West",
-  data_long$name=="Gateshead" ~ "North East",
-  TRUE ~ data_long$region
+data$region <- case_when(
+  data$name=="Dorset" ~ "South West",
+  data$name=="Bournemouth, Christchurch and Poole" ~ "South West",
+  data$name=="Gateshead" ~ "North East",
+  data$name=="City of London" ~ "London",
+  TRUE ~ data$region
 )
 
-#Bring in population data 
+#Bring in LA-level population data
 temp <- tempfile()
 source <- "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fpopulationandmigration%2fpopulationestimates%2fdatasets%2fpopulationestimatesforukenglandandwalesscotlandandnorthernireland%2fmid20182019laboundaries/ukmidyearestimates20182019ladcodes.xls"
 temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
@@ -65,7 +63,6 @@ LApop_long <- gather(LApop, age, pop, pop)
 #deal with Cornwall & Scilly + Hackney & City of London which are merged in the PHE data
 LApop_long$code <- case_when(
   LApop_long$code2=="E06000052" ~ "E06000052",
-  LApop_long$code2=="E09000001" ~ "E09000012",
   TRUE ~ LApop_long$code2
 )
 
@@ -74,25 +71,47 @@ LApop_long <- LApop_long %>%
   summarise(pop=sum(pop))
 
 #Combine
-data_long <- merge(data_long, LApop_long, all.x=TRUE)
+data <- merge(data, LApop_long, all.x=TRUE)
 
+#Set up skepeton dataframe with dates
+LAcodes <- unique(data$code)
+min <- min(data$date)
+max <- max(data$date)
+
+skeleton <- data.frame(code=rep(LAcodes, each=(max-min+1), times=1), date=rep(seq.Date(from=min, to=max, by="day"), each=1, times=length(LAcodes)))
+
+#Map data onto skeleton
+fulldata <- merge(skeleton, data[,c(1,4:6)], by=c("code", "date"), all.x=TRUE, all.y=TRUE)
+
+#Fill in blank days
+fulldata$cases <- ifelse(is.na(fulldata$cases), 0, fulldata$cases)
+
+#Calculate cumulative sums so far
+fulldata <- fulldata %>%
+  group_by(code) %>%
+  mutate(cumul_cases=cumsum(cases))
+
+#Bring in LA factors
+LAdata <- unique(data[,c(1,2,7:11)])
+fulldata <- merge(fulldata, LAdata)
+  
 #Calculate rates by LA
-data_long$caserate <- data_long$cases*100000/data_long$pop
+fulldata$caserate <- fulldata$cases*100000/fulldata$pop
 
 #Collapse to quintiles
-quintiles <- data_long %>%
+quintiles <- fulldata %>%
   group_by(quintile, date) %>%
-  summarise(cases=sum(cases), pop=sum(pop))
+  summarise(cases=sum(cases), cumul_cases=sum(cumul_cases), pop=sum(pop))
 
 #Calculate rates
-quintiles$caserate <- quintiles$cases*100000/quintiles$pop
+quintiles$caserate <- quintiles$cumul_cases*100000/quintiles$pop
 
 #Set up lines for log breaks
-logbreaks <- data.frame(breaks=c(10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000))
+logbreaks <- data.frame(breaks=c(1,2,5,10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000))
 ratebreaks <- data.frame(breaks=c(1,2,5,10,20,50))
 
 tiff("Outputs/COVIDQuintiles.tiff", units="in", res=300, width=8, height=6)
-ggplot(quintiles, aes(x=date, y=cases, colour=as.factor(quintile)))+
+ggplot(quintiles, aes(x=date, y=cumul_cases, colour=as.factor(quintile)))+
   geom_line()+
   theme_classic()+
   scale_colour_manual(values=c("#fcc5c0", "#fa9fb5", "#f768a1", "#c51b8a", "#7a0177"), name="IMD quintile",
@@ -104,7 +123,7 @@ ggplot(quintiles, aes(x=date, y=cases, colour=as.factor(quintile)))+
 dev.off()
 
 tiff("Outputs/COVIDQuintilesLog.tiff", units="in", res=300, width=8, height=6)
-ggplot(quintiles, aes(x=date, y=cases, colour=as.factor(quintile)))+
+ggplot(subset(quintiles, cumul_cases>0), aes(x=date, y=cumul_cases, colour=as.factor(quintile)))+
   geom_segment(data=logbreaks, aes(x=as.Date(-Inf, origin="2018-01-01"), xend=as.Date(Inf, origin="2018-01-01"), 
                                    y=breaks, yend=breaks), colour="gray80")+
   geom_line()+
@@ -130,9 +149,9 @@ ggplot(quintiles, aes(x=date, y=caserate, colour=as.factor(quintile)))+
 dev.off()
 
 tiff("Outputs/COVIDQuintilesRateLog.tiff", units="in", res=300, width=8, height=6)
-ggplot(quintiles, aes(x=date, y=caserate, colour=as.factor(quintile)))+
+ggplot(subset(quintiles, cumul_cases>0), aes(x=date, y=caserate, colour=as.factor(quintile)))+
   geom_segment(data=ratebreaks, aes(x=as.Date(-Inf, origin="2018-01-01"), xend=as.Date(Inf, origin="2018-01-01"), 
-                                   y=breaks, yend=breaks), colour="gray80")+  geom_line()+
+                                    y=breaks, yend=breaks), colour="gray80")+  geom_line()+
   theme_classic()+
   scale_colour_manual(values=c("#fcc5c0", "#fa9fb5", "#f768a1", "#c51b8a", "#7a0177"), name="IMD quintile",
                       labels=c("Q1 (least deprived)", "Q2", "Q3", "Q4", "Q5 (most deprived)"))+
@@ -144,21 +163,21 @@ ggplot(quintiles, aes(x=date, y=caserate, colour=as.factor(quintile)))+
 dev.off()
 
 #Split into London/not London
-data_long$London <- case_when(
-  data_long$region=="London" ~ "London",
+fulldata$London <- case_when(
+  fulldata$region=="London" ~ "London",
   TRUE ~ "Rest of England"
 )
 
 #Collapse to quintiles
-Lonquintiles<- data_long %>%
+Lonquintiles<- fulldata %>%
   group_by(quintile, London, date) %>%
-  summarise(cases=sum(cases), pop=sum(pop))
+  summarise(cases=sum(cases), cumul_cases=sum(cumul_cases), pop=sum(pop))
 
 #Calculate rates
-Lonquintiles$caserate <- Lonquintiles$cases*100000/Lonquintiles$pop
+Lonquintiles$caserate <- Lonquintiles$cumul_cases*100000/Lonquintiles$pop
 
 tiff("Outputs/COVIDQuintilesLon.tiff", units="in", res=300, width=12, height=6)
-ggplot(Lonquintiles, aes(x=date, y=cases, colour=as.factor(quintile)))+
+ggplot(Lonquintiles, aes(x=date, y=cumul_cases, colour=as.factor(quintile)))+
   geom_line()+
   facet_wrap(~London)+
   theme_classic()+
@@ -171,9 +190,9 @@ ggplot(Lonquintiles, aes(x=date, y=cases, colour=as.factor(quintile)))+
 dev.off()
 
 tiff("Outputs/COVIDQuintilesLonLog.tiff", units="in", res=300, width=12, height=6)
-ggplot(Lonquintiles, aes(x=date, y=cases, colour=as.factor(quintile)))+
+ggplot(subset(Lonquintiles, cumul_cases>0), aes(x=date, y=cumul_cases, colour=as.factor(quintile)))+
   geom_segment(data=logbreaks, aes(x=as.Date(-Inf, origin="2018-01-01"), xend=as.Date(Inf, origin="2018-01-01"), 
-                                    y=breaks, yend=breaks), colour="gray80")+  geom_line()+  geom_line()+
+                                   y=breaks, yend=breaks), colour="gray80")+  geom_line()+  geom_line()+
   facet_wrap(~London)+
   theme_classic()+
   scale_colour_manual(values=c("#fcc5c0", "#fa9fb5", "#f768a1", "#c51b8a", "#7a0177"), name="IMD quintile",
@@ -199,7 +218,7 @@ ggplot(Lonquintiles, aes(x=date, y=caserate, colour=as.factor(quintile)))+
 dev.off()
 
 tiff("Outputs/COVIDQuintilesLonRateLog.tiff", units="in", res=300, width=12, height=6)
-ggplot(Lonquintiles, aes(x=date, y=caserate, colour=as.factor(quintile)))+
+ggplot(subset(Lonquintiles, cumul_cases>0), aes(x=date, y=caserate, colour=as.factor(quintile)))+
   geom_segment(data=ratebreaks, aes(x=as.Date(-Inf, origin="2018-01-01"), xend=as.Date(Inf, origin="2018-01-01"), 
                                     y=breaks, yend=breaks), colour="gray80")+  geom_line()+  geom_line()+
   facet_wrap(~London)+
@@ -219,19 +238,19 @@ dev.off()
 shapefile <- st_read("Shapefiles/Counties_and_Unitary_Authorities_December_2017_Full_Clipped_Boundaries_in_UK.shp")
 names(shapefile)[names(shapefile) == "ctyua17cd"] <- "code"
 
-#Duplicate data to account for shapefile using pre-2019 codes
-int1 <- filter(data_long, name=="Bournemouth, Christchurch and Poole")
-int1$code <- "E06000028"
-int2 <- filter(data_long, name=="Bournemouth, Christchurch and Poole")
-int2$code <- "E06000029"
-int3 <- filter(data_long, name=="Bournemouth, Christchurch and Poole")
-int3$code <- "E10000009"
-int4 <- filter(data_long, name=="Cornwall and Isles of Scilly")
-int4$code <- "E06000053"
-int5 <- filter(data_long, name=="Hackney and City of London")
-int5$code <- "E09000001"
+fulldata$caserate <- fulldata$cumul_cases*100000/fulldata$pop
 
-temp <- rbind(data_long, int1, int2, int3, int4, int5)
+#Duplicate data to account for shapefile using pre-2019 codes
+int1 <- filter(fulldata, name=="Bournemouth, Christchurch and Poole")
+int1$code <- "E06000028"
+int2 <- filter(fulldata, name=="Bournemouth, Christchurch and Poole")
+int2$code <- "E06000029"
+int3 <- filter(fulldata, name=="Bournemouth, Christchurch and Poole")
+int3$code <- "E10000009"
+int4 <- filter(fulldata, name=="Cornwall and Isles of Scilly")
+int4$code <- "E06000053"
+
+temp <- rbind(fulldata, int1, int2, int3, int4)
 
 #Bring in data
 map.data <- full_join(shapefile, temp, by="code", all.y=TRUE)
@@ -251,7 +270,7 @@ England <- ggplot(data=subset(map.data, date==max(date)), aes(fill=caserate, geo
   labs(title="Confirmed COVID-19 case rates for Local Authorities in England",
        caption="Data from Public Health England | Plot by @VictimOfMaths")+
   geom_rect(aes(xmin=500000, xmax=560000, ymin=156000, ymax=200000), fill="transparent",
-                colour="gray50")+
+            colour="gray50")+
   geom_rect(aes(xmin=310000, xmax=405000, ymin=370000, ymax=430000), fill="transparent",
             colour="gray50")+
   geom_rect(aes(xmin=405000, xmax=490000, ymin=505000, ymax=580000), fill="transparent",
@@ -264,7 +283,7 @@ London <- ggplot(data=subset(map.data, date==max(date)), aes(fill=caserate, geom
   ylim(156000,200000)+
   theme_classic()+
   scale_fill_paletteer_c("pals::ocean.tempo", name="Total confirmed\ncases per 100,000",
-                         )+
+  )+
   labs(title="Greater London")+
   theme(axis.line=element_blank(), axis.ticks=element_blank(), axis.text=element_blank(),
         axis.title=element_blank())
