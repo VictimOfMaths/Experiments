9298edea008e3bad574a75bce42ecbde9387e87c
Colin Angus <c.r.angus@sheffield.ac.uk>
2022-05-19 14:23:21 +0100

    Update LeagueWinners.R

--- LeagueWinners.R
+++ LeagueWinners.R
@@ -3,7 +3,7 @@ rm(list=ls())
 library(tidyverse)
 library(xml2)
 library(rvest)
-library(lubridate)
+library(paletteer)
 library(stringr)
 library(extrafont)
 library(ragg)
@@ -113,7 +113,54 @@ PrimeiraLiga <- as.data.frame(html_table(temp[2]))[,c(2,3)] %>%
   slice(-c(1:61)) %>% 
   filter(Season!="Primeir")
 
-data <- bind_rows(EPL, Ligue1, LaLiga, SerieA, Bundesliga, PrimeiraLiga) %>% 
+#Eredivisie
+url <- "https://en.wikipedia.org/wiki/List_of_Dutch_football_champions"
+
+#Grab html tables 
+temp <- url %>% read_html %>% html_nodes("table")
+
+#Tidy them up 
+Eredivisie <- as.data.frame(html_table(temp[3]))[,c(1,2)] %>% 
+  set_names("Season", "Winners") %>% 
+  mutate(Winners=gsub(" \\(.*", "", Winners),
+         Season=substr(Season, 1, 7),
+         league="Eredivisie") %>% 
+  slice(-c(1:36)) %>% 
+  filter(Season!="2019–20")
+
+#Scotland
+url <- "https://en.wikipedia.org/wiki/List_of_Scottish_football_champions"
+
+#Grab html tables 
+temp <- url %>% read_html %>% html_nodes("table")
+
+#Tidy them up 
+Scotland <- as.data.frame(html_table(temp[7]))[,c(1,2)] %>% 
+  bind_rows(as.data.frame(html_table(temp[8]))[,c(1,2)]) %>% 
+  bind_rows(as.data.frame(html_table(temp[9]))[,c(1,2)]) %>% 
+  set_names("Season", "Winners") %>% 
+  mutate(Winners=gsub(" \\(.*", "", Winners),
+         Season=substr(Season, 1, 7),
+         league="Scottish Premiership") %>% 
+  filter(Season!="Season") %>% 
+  slice(-c(1:17))  
+
+#MLS
+url <- "https://en.wikipedia.org/wiki/List_of_American_and_Canadian_soccer_champions"
+
+#Grab html tables 
+temp <- url %>% read_html %>% html_nodes("table")
+
+#Tidy them up 
+MLS <- as.data.frame(html_table(temp[4]))[,c(1,2)] %>% 
+  set_names("Season", "Winners") %>% 
+  mutate(Winners=gsub(" \\(.*", "", Winners),
+         Season=substr(Season, 1, 7),
+         league="MLS") %>% 
+  filter(Season!="Season") 
+
+data <- bind_rows(EPL, Ligue1, LaLiga, SerieA, Bundesliga, PrimeiraLiga, Eredivisie,
+                  Scotland, MLS) %>% 
   group_by(league, Winners) %>% 
   summarise(wins=n()) %>% 
   ungroup()
@@ -125,6 +172,41 @@ ggplot(data, aes(x=wins, y=fct_reorder(Winners, wins), fill=wins))+
   scale_y_discrete(name="")+
   scale_fill_paletteer_c("viridis::rocket", direction=-1)+
   facet_wrap(~league, scales="free_y")+
+  theme_custom()
+
+dev.off()
+
+data2 <- bind_rows(EPL, Ligue1, LaLiga, SerieA, Bundesliga, PrimeiraLiga, Eredivisie,
+                   Scotland, MLS) %>% 
+  mutate(year=as.numeric(substr(Season, 1, 4))) %>% 
+  group_by(league) %>% 
+  mutate(change=if_else(Winners==lag(Winners, 1), "No change", "Change")) %>% 
+  ungroup()
+
+data3 <- data2 %>% 
+  group_by(league, change) %>% 
+  summarise(changes=n()) %>% 
+  ungroup()
+
+agg_png("Outputs/LeagueChanges.png", units="in", width=9, height=8, res=800)
+ggplot(data2 %>% filter(year>1992), aes(x=year, y=league, fill=change))+
+  geom_tile()+
+  scale_x_continuous(name="")+
+  scale_y_discrete(name="")+
+  scale_fill_manual(values=c("#EBC915", "#018AC4"), na.value="White", name="Title holders",
+                    labels=c("Change", "No change", ""))+
+  theme_custom()
+
+dev.off()
+
+
+agg_png("Outputs/LeagueChanges2.png", units="in", width=9, height=8, res=800)
+ggplot(data3, aes(x=changes, y=league, fill=change))+
+  geom_col()+
+  scale_x_continuous(name="")+
+  scale_y_discrete(name="")+
+  scale_fill_manual(values=c("#EBC915", "#018AC4"), na.value="White", name="Title holders",
+                    labels=c("Change", "No change", ""))+
   theme_custom()
 
 dev.off()
